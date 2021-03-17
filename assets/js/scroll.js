475c3a2fb77c4eeb8a5477a3ff2ae20754c355f2
Tianyi Ge <tianyig@outlook.com>
2021-03-18 02:07:13 +0800

    footer

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($(document).scrollTop() > 100) {
+        if ($(document).scrollTop() > 40) {
             $("nav").addClass("scrolled");
         } else {
             $("nav").removeClass("scrolled");
         }
     }); 
 });
-
