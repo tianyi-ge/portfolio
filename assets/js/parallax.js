2bb55c90db219d2ec5b00f28e4082d0a8801205b
Tianyi Ge <tianyig@outlook.com>
2021-04-21 13:51:04 +0800

    update style

--- assets/js/parallax.js
+++
@@ -1,6 +0,0 @@
-// const parallax = document.getElementsByClassName("bg");
-
-// window.addEventListener("scroll", () => {
-//     let offset = window.pageYOffset;
-//     parallax[0].style.backgroundPositionY = offset * 0.8 + "px";
-// });
