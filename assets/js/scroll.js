c771e8d4225ae9882913b1359d580a96ed215570
Tianyi Ge <tianyig@outlook.com>
2021-04-21 19:33:04 +0800

    detect window width for navbar

--- assets/js/scroll.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > 40) {
-            $("nav").addClass("scrolled");
-        } else {
-            $("nav").removeClass("scrolled");
-        }
-    }); 
-});
